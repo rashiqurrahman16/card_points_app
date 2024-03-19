f7716a4651f6c82021f43d34e6444ac80c0457c4
rashiqurrahman16 <rashiqur.rahman16@gmail.com>
2024-03-19 12:48:32 +0600

    history page added

---
+++ lib/pages/history_page.dart
@@ -0,0 +1,287 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:hazari/pages/about_us.dart';
+import 'package:hazari/pages/rules_page.dart';
+import 'package:hazari/widgets/exit_confirmation_page.dart';
+
+import '../boxes/boxes.dart';
+import '../models/name_score_model.dart';
+
+class HistoryPage extends StatefulWidget {
+  const HistoryPage({super.key});
+
+  @override
+  State<HistoryPage> createState() => _HistoryPageState();
+}
+
+class _HistoryPageState extends State<HistoryPage> {
+  List<FinalScoreModel> finalScoresList = [];
+  @override
+  void initState() {
+    super.initState();
+    _retrieveNamesScores();
+  }
+
+  void _retrieveNamesScores() async {
+    final box3 = Boxes.getFinalScores();
+    final finalScores = box3.values.toList().cast<FinalScoreModel>();
+
+    setState(() {
+      finalScoresList = finalScores;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize=MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            SizedBox(
+              width: 35.0,
+              height: 35.0,
+              child: Image(
+                image: AssetImage('assets/only_cards.png'),
+              ),
+            ),
+            SizedBox(width: 5),
+            Text(
+              'হাজারি',
+              style: TextStyle(color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w400,),
+            ),
+            Spacer(),
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => AboutUsPage()),
+                    );
+                  },
+                  child:  SizedBox(
+                    width: 35.0,
+                    height: 35.0,
+                    child: Image(
+                      image: AssetImage('assets/Logo.png'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        toolbarHeight: 80,
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        automaticallyImplyLeading: false,
+      ),
+      body: ListView(
+        children: [
+          ElevatedButton(onPressed: (){
+            _clearHistory();
+          }, child: Text("Clear all")),
+          SizedBox(
+            height: screenSize.height - 120,
+            child: ListView.builder(
+              itemCount: finalScoresList.length,
+              itemBuilder: (context, index) {
+                final finalScoreData = finalScoresList[index];
+                return ListTile(
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(finalScoreData.finalPlayer1)
+                              ],
+                            ),
+                          ),
+                          Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(finalScoreData.finalPlayer2)
+                              ],
+                            ),
+                          ),
+                          Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(finalScoreData.finalPlayer3)
+                              ],
+                            ),
+                          ),
+                          Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(finalScoreData.finalPlayer4)
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: screenSize.width/4,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(finalScoreData.finalScore1.toString()),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            width: screenSize.width/4,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(finalScoreData.finalScore2.toString()),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            width: screenSize.width/4,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(finalScoreData.finalScore3.toString()),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            width: screenSize.width/5,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(finalScoreData.finalScore4.toString()),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }
+            ),
+          ),
+        ]
+      ),
+
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.blue,
+          shape: const CircularNotchedRectangle(),
+          child: Container(
+            alignment: Alignment.center,
+            height: 10,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue.shade400,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        side: BorderSide(
+                          color: Colors.blue.shade600,  // Set border color to white
+                          width: 2.0,           // Adjust border width as desired
+                        ),
+                      ),
+                    ),
+                    onPressed: () => Navigator.pop(context),
+                    child: Text("Back",
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                )),
+
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue.shade400,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        side: BorderSide(
+                          color: Colors.blue.shade600,  // Set border color to white
+                          width: 2.0,           // Adjust border width as desired
+                        ),
+                      ),
+                    ),
+                    onPressed: () async {
+                      await ExitConfirmationPage().showExitConfirmationDialog(context);
+                    }, child: Text("Exit Game",
+                  style: TextStyle(color: Colors.white, fontSize: 14),
+                )),
+              ],
+            ),
+
+          ),
+        )
+
+    );
+  }
+
+  Future<void> _clearHistory() async{
+    return showDialog(
+        context: context,
+        builder: (context){
+          return AlertDialog(
+            title: Center(child: Text('Are you sure want to clear the history?', style: TextStyle(fontSize: 18),)),
+            actions: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 50,
+                    width: 90,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 4),
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(backgroundColor: Colors.orange.shade400),
+                          onPressed: () async{
+                            Navigator.pop(context);
+                          },
+                          child: Text('Cancel', style: TextStyle(color: Colors.white, fontSize: 18),)
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 20,),
+                  SizedBox(
+                    height: 50,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 4),
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(backgroundColor: Colors.green.shade500),
+                          onPressed: () async {
+                            final box3 = Boxes.getFinalScores();
+                            await box3.clear();
+                            setState(() {
+                              finalScoresList = [];
+                            });
+                            onPressed: () => Navigator.pop(context);
+                          },
+                          child: Text('Confirm', style: TextStyle(color: Colors.white, fontSize: 18),)
+                      ),
+                    ),
+                  )
+                ],
+              )
+
+            ],
+          );
+        });
+  }
+
+}
